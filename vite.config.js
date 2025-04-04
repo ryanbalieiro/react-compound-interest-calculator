f5accc3bdc299465ace1a14de744a78376e2de0b
Ryan Balieiro <ryan.balieiro@gmail.com>
2025-04-03 23:38:58 -0300

    - Added workflow to deploy the app automatically. - Updated Bootstrap to 5.3.4 and Vite to 6.2.5.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/react-compound-interest-calculator/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 500
+  },
   css: {
     preprocessorOptions: {
       scss: {
